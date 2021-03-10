bf173d47aeeecce17452617c72326c7567b145fa
Victor Fuentes Campos <57545004+vfuentesc@users.noreply.github.com>
2021-03-09 20:27:53 -0600

    Create project.R

---
+++ project.R
@@ -0,0 +1,41 @@
+rm(list=ls())
+
+library(tidyverse)
+library(tidytext)
+
+# Victor
+wd <- "C:/Users/vfuentesc/OneDrive - The University of Chicago/Winter 2021/Data & Programming II - R/Project/final_project-fuentes-garcia"
+
+# Fernando
+#wd <- "C:/Users/vfuentesc/OneDrive - The University of Chicago/Winter 2021/Data & Programming II - R/Project/final_project-fuentes-garcia"
+
+setwd(wd)
+
+###################################
+### Data Wrangling ################
+###################################
+
+
+
+
+
+###################################
+### Plotting ######################
+###################################
+
+
+
+
+
+
+###################################
+### Text Processing ###############
+###################################
+
+
+
+
+
+###################################
+### Analysis ######################
+###################################
